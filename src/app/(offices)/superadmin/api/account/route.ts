ab1a5463bfad3ec0a7411b04332547fee3efa650
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-31 18:11:06 +0800

    minor

--- src/app/(offices)/superadmin/api/account/route.ts
+++ src/app/(offices)/superadmin/api/account/route.ts
@@ -1,6 +1,6 @@
 'use server';
 import connectDB from "@/lib/database";
-import { Roles, UserDocument } from "@/lib/modelInterfaces";
+import { Roles } from "@/lib/modelInterfaces";
 import PhotoFile from "@/lib/models/PhotoFile";
 import User from "@/lib/models/User";
 import { getSession } from "@/lib/session";
@@ -11,9 +11,14 @@ export async function GET(request: NextRequest) {
   try {
     const session = await getSession(Roles.SuperAdmin)
     if (!!session?.user) {
-      const users = await User.findById({ _id: session.user._id}).select('-password -departmentIds -readMemos -readLetters -deactivated -notification').exec()
-      const result = await Promise.all(JSON.parse(JSON.stringify(users)).map(async (user: UserDocument) => ({...user, photo: user.photo? JSON.parse(JSON.stringify(await PhotoFile.findById(user.photo))) : undefined })));
-      return NextResponse.json({ result: users })
+      console.log("SESSION", session?.user)
+      const users = await User.findOne({ _id: session.user._id }).select('-password -departmentIds -readMemos -readLetters -deactivated -notification').exec()
+      console.log("USER", users)
+      const parsed = JSON.parse(JSON.stringify(users))
+      const photo = parsed?.photo ? await PhotoFile.findById(parsed.photo) : undefined
+      parsed.photo = photo
+      const result = JSON.parse(JSON.stringify(parsed))
+      return NextResponse.json({ result })
     }
   } catch (e) {}
   return NextResponse.json({ result: {} })
